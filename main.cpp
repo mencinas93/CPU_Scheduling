d9e339ae4212eb63fb27af7ac71345d35d954aed
Michael Encinas <115640100+mencinas93@users.noreply.github.com>
2023-04-14 13:19:12 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,114 +25,93 @@
  Because round robin is deterministic, you should see the same job scheduling result every time you run your code.
 
  */
-#include <fstream>
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <sstream>
 #include <vector>
 
-
 using namespace std;
-//job class with constructor of arrtime,burst and name
-class Job{
-public: Job(int id, int arrival_time,int burst_time): id_(id), arrival_time_(arrival_time), burst_time_(burst_time), remaining_time_(burst_time){}
-    int id() const {
-        return id_;
+
+class Job {
+public:
+    Job(int id, int arrival_time, int duration)
+            : job_id(id), arrival_time(arrival_time), duration(duration), remaining_time(duration) {}
+
+    int job_id;
+    int arrival_time;
+    int duration;
+    int remaining_time;
+
+    void print_scheduled(int current_time) {
+        cout << "$> Job " << job_id << ", scheduled for " << current_time << "ms" << endl;
     }
-    int arrival_time() const {
-        return arrival_time_;
+
+    void print_completed() {
+        cout << "$> Job " << job_id << ", completed" << endl;
     }
-    int burst_time() const {
-        return burst_time_;
-    }
-    int remaining_time() const {
-            return remaining_time_;
-        }
-    //remainder time
-        void set_remaining_time(int remaining_time) {
-            remaining_time_ = remaining_time;
-        }
-    //updates the status of remaining time.
-    //public access to private
-private:
-    int id_;
-    int arrival_time_;
-    int burst_time_;
-    int remaining_time_;
 };
 
-class Scheduler {
-public:
-    Scheduler(vector<Job>& jobs) : jobs_(jobs) {}
+// Function to simulate round-robin scheduling
+void round_robin_scheduling(vector<Job>& jobs, int time_quantum) {
+    unsigned int n = jobs.size();
+    int current_time = 0;
+    vector<Job*> completed_jobs;
+    int idx = 0;
 
-    void run() {
-        int current_time = 0;
+    while (completed_jobs.size() < n) {
+        Job& job = jobs[idx];
 
-        while (!jobs_.empty()) {
-            Job& job = jobs_.front();
+        if (job.remaining_time > 0) {
+            if (job.arrival_time <= current_time) {
+                job.print_scheduled(current_time);
+                job.remaining_time -= time_quantum;
+                current_time += time_quantum;
 
-            if (job.remaining_time() <= 10) {
-                cout << "$>Job " << job.id() << ", scheduled for " << job.remaining_time() << "ms, completed" << endl;
-                current_time += job.remaining_time();
-                jobs_.erase(jobs_.begin());
-            } else {
-                cout << "$>Job " << job.id() << ", scheduled for 10ms" << endl;
-                current_time += 10;
-                job.set_remaining_time(job.remaining_time()-10);
-
-                // Move the job to the back of the queue
-                jobs_.push_back(job);
-                jobs_.erase(jobs_.begin());            }
-            for (int i = 0; i < jobs_.size(); ) {
-                if (jobs_[i].arrival_time() <= current_time) {
-                    cout << "$>Job " << jobs_[i].id() << ", scheduled for " << jobs_[i].burst_time() - jobs_[i].remaining_time() << "ms" << endl;
-                    //add the job to the front of the queue
-                    jobs_.insert(jobs_.begin(), jobs_[i]);
-                    jobs_.erase(jobs_.begin() + i);
-                    i = 0;
-                } else {
-                    i++;
-            // Check if any new jobs have arrived
-           
+                if (job.remaining_time <= 0) {
+                    job.print_completed();
+                    completed_jobs.push_back(&job);
                 }
+            } else {
+                current_time++;
             }
         }
-    }
 
-private:
-    vector<Job>& jobs_;
-};
+        idx = (idx + 1) % n; // Move to the next job in a circular manner
+    }
+}
 
 int main() {
-    ifstream iFile;
-    
-    iFile.open("job.txt");
-    if (!iFile.is_open()){
-        cout << "Not inputting file!/n";
+    string file_name = "job.txt"; // Replace with the name of your input file
+    int time_quantum = 10; // Time quantum for round-robin scheduling
+
+    // Read jobs from input file
+    ifstream file(file_name);
+    if (!file.is_open()) {
+        cout << "Failed to open file: " << file_name << endl;
+        return 1;
     }
+
     vector<Job> jobs;
-    //vector to place job lines
-
-    string line;
-    int id = 1;
-    // name of ID
-    while(getline(iFile,line)){
-        int arrival_time,burst_time;
-        if(sscanf(line.c_str(), "%d,%d,%d", &id, &arrival_time, &burst_time) != 3) {
-            //reads three integers separated by commas
-            //turns string input to integer
-            cout << "Invalid input line: " << line << endl;
-// if cant be inputted error alert
-            continue;
+    string job_line;
+    while (getline(file, job_line)) {
+        stringstream ss(job_line);
+        string job_id_str, arrival_time_str, duration_str;
+        if (getline(ss, job_id_str, ',') &&
+            getline(ss, arrival_time_str, ',') &&
+            getline(ss, duration_str, ',')) {
+            int job_id = stoi(job_id_str);
+            int arrival_time = stoi(arrival_time_str);
+            int duration = stoi(duration_str);
+            Job job(job_id, arrival_time, duration);
+            jobs.push_back(job);
+        } else {
+            cout << "Failed to parse line: " << job_line << endl;
         }
-        jobs.push_back(Job(id++, arrival_time, burst_time));
     }
-    Scheduler scheduler(jobs);
-      scheduler.run();
+    file.close();
 
-    
-    for (const auto& job : jobs) {
-            cout << "Job " << job.id() << ": Arrival Time " << job.arrival_time() << ", Burst Time " << job.burst_time() << endl;
-        }
-     
+    // Perform round-robin scheduling
+    round_robin_scheduling(jobs, time_quantum);
+
     return 0;
 }
